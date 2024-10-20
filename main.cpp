9b9fa33a1f66d6800e762414c0650b7dedc676fb
piotr-zeberek-agh <zeberek@student.agh.edu.pl>
2024-10-20 19:42:01 +0200

    cl args version

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <unsupported/Eigen/KroneckerProduct>
 
 // LAO/STO 001
-int main()
+int main(int argc, char *argv[])
 {
     Parameters p;
 
@@ -99,8 +99,8 @@ int main()
     {
         // System2D sys(HBdG, p);
 
-        // sys._p.mu = meV2au(4.0);
-        // sys._p.Bz = T2au(5.0);
+        // sys._p.mu = meV2au(-2.0);
+        // sys._p.Bz = T2au(2.0);
 
         // Eigen::VectorXd kx_vec = Eigen::VectorXd::LinSpaced(10001, -0.5, 0.5);
 
@@ -148,47 +148,82 @@ int main()
 
     // Chern numbers vs mu and B
     {
+        // System2D sys(HBdG, p);
+
+        // std::size_t n_dense = 4000; // number of k-points in each direction - even,
+        // std::size_t n_sparse = 1000; // number of k-points in each direction - even,
+
+        // double mu_max = meV2au(4.0);
+        // double Bz_max = T2au(5);
+
+        // Eigen::VectorXd mu_vec = Eigen::VectorXd::LinSpaced(5, -mu_max, mu_max);
+        // Eigen::VectorXd Bz_vec = Eigen::VectorXd::LinSpaced(6, -Bz_max, Bz_max);
+
+        // std::ofstream mu_Bz_Cs_file("data/mu_Bz_Cs.dat");
+
+        // for (auto i = 0; i < mu_vec.size(); ++i)
+        // {
+        //     for (auto j = 0; j < Bz_vec.size(); ++j)
+        //     {
+        //         sys._p.mu = mu_vec(i);
+        //         sys._p.Bz = Bz_vec(j);
+        //         mu_Bz_Cs_file << mu_vec(i) / meV2au(1.0) << "\t"
+        //                       << Bz_vec(j) / T2au(1.0) << "\t"
+        //                       << sys.calcChernNumbersDenserCenter(n_dense, n_sparse, 0.45).transpose() << std::endl;
+        //         std::cout << i * Bz_vec.size() + j + 1 << " out of " << mu_vec.size() * Bz_vec.size() << std::endl;
+        //     }
+        // }
+    }
+
+    // Chern numbers vs mu and B - command line arguments version
+    {
         System2D sys(HBdG, p);
 
-        std::size_t n_dense = 4000; // number of k-points in each direction - even,
-        std::size_t n_sparse = 1000; // number of k-points in each direction - even,
+        std::size_t n_dense = 5000;  // number of k-points in each direction - even,
+        std::size_t n_sparse = 1500; // number of k-points in each direction - even,
 
-        double mu_max = meV2au(4.0);
-        double Bz_max = T2au(5);
-
-        Eigen::VectorXd mu_vec = Eigen::VectorXd::LinSpaced(5, -mu_max, mu_max);
-        Eigen::VectorXd Bz_vec = Eigen::VectorXd::LinSpaced(6, -Bz_max, Bz_max);
-
-        std::ofstream mu_Bz_Cs_file("data/mu_Bz_Cs.dat");
-
-        for (auto i = 0; i < mu_vec.size(); ++i)
+        switch (argc)
         {
-            for (auto j = 0; j < Bz_vec.size(); ++j)
-            {
-                sys._p.mu = mu_vec(i);
-                sys._p.Bz = Bz_vec(j);
-                mu_Bz_Cs_file << mu_vec(i) / meV2au(1.0) << "\t"
-                              << Bz_vec(j) / T2au(1.0) << "\t"
-                              << sys.calcChernNumbersDenserCenter(n_dense, n_sparse, 0.45).transpose() << std::endl;
-                std::cout << i * Bz_vec.size() + j + 1 << " out of " << mu_vec.size() * Bz_vec.size() << std::endl;
-            }
+        case 2:
+            sys._p.mu = meV2au(std::stod(argv[1]));
+            break;
+        case 3:
+            sys._p.mu = meV2au(std::stod(argv[1]));
+            sys._p.Bz = T2au(std::stod(argv[2]));
+            break;
+        case 5:
+            sys._p.mu = meV2au(std::stod(argv[1]));
+            sys._p.Bx = T2au(std::stod(argv[2]));
+            sys._p.By = T2au(std::stod(argv[3]));
+            sys._p.Bz = T2au(std::stod(argv[4]));
+            break;
+        default:
+            std::cerr << "Usage: " << argv[0] << " mu (meV)" << std::endl;
+            std::cerr << "Usage: " << argv[0] << " mu (meV) Bz (T)" << std::endl;
+            std::cerr << "Usage: " << argv[0] << " mu (meV) Bx By Bz (T)" << std::endl;
+            return 1;
         }
+
+        std::cout << "mu = " << sys._p.mu / meV2au(1.0) << " meV" << std::endl;
+        std::cout << "Bx = " << sys._p.Bx / T2au(1.0) << " T" << std::endl;
+        std::cout << "By = " << sys._p.By / T2au(1.0) << " T" << std::endl;
+        std::cout << "Bz = " << sys._p.Bz / T2au(1.0) << " T" << std::endl;
+
+        std::cout << "Chern numbers: " << sys.calcChernNumbersDenserCenter(n_dense, n_sparse, 0.5).transpose() << std::endl;
     }
 
     // BC
     {
         // System2D sys(HBdG, p);
-        // sys._p.mu = meV2au(-2.89);
+        // sys._p.mu = meV2au(-2);
+        // sys._p.Bz = T2au(5);
 
-        // std::size_t n_k = 2001;
-        // Eigen::VectorXd kx_vec = Eigen::VectorXd::LinSpaced(n_k, -M_PI, M_PI);
-        // Eigen::VectorXd ky_vec = Eigen::VectorXd::LinSpaced(n_k, -M_PI, M_PI);
+        // std::size_t n_k = 1001;
+        // Eigen::VectorXd kx_vec = Eigen::VectorXd::LinSpaced(n_k, -0.5, 0.5);
+        // Eigen::VectorXd ky_vec = Eigen::VectorXd::LinSpaced(n_k, -0.5, 0.5);
 
-        // // analityczne pochodne - jest zle chyba
-        // sys.printBerryCurvature("data/BC.dat", dvxHBdG, dvyHBdG, kx_vec, ky_vec);
-
-        // sys.printBerryCurvature("data/BC_num.dat", dvxHBdG_num, dvyHBdG_num, kx_vec, ky_vec);
-        // sys.printBerryCurvature("data/BC_num2.dat", dvxHBdG_num2, dvyHBdG_num2, kx_vec, ky_vec);
+        // sys.printBerryCurvature("data/BC.dat", dvxHBdG_num, dvyHBdG_num, kx_vec, ky_vec);
+        // // sys.printBerryCurvature("data/BC_num2.dat", dvxHBdG_num2, dvyHBdG_num2, kx_vec, ky_vec);
     }
 
     // gap vs momentum
@@ -387,30 +422,30 @@ int main2()
 
     // gap vs momentum
     {
-        System2D sys(HBdG, p);
+        // System2D sys(HBdG, p);
 
-        std::size_t n_k = 1001;
-        Eigen::VectorXd kx_vec = Eigen::VectorXd::LinSpaced(n_k, -1.5, 1.5);
-        Eigen::VectorXd ky_vec = Eigen::VectorXd::LinSpaced(n_k, -1.5, 1.5);
+        // std::size_t n_k = 1001;
+        // Eigen::VectorXd kx_vec = Eigen::VectorXd::LinSpaced(n_k, -1.5, 1.5);
+        // Eigen::VectorXd ky_vec = Eigen::VectorXd::LinSpaced(n_k, -1.5, 1.5);
 
-        sys._p.mu = meV2au(0.0);
+        // sys._p.mu = meV2au(0.0);
 
-        sys.printGap("data/gap.dat", kx_vec, kx_vec);
+        // sys.printGap("data/gap.dat", kx_vec, kx_vec);
     }
 
     // FS contours and gap along them
     {
-        System2D sys(HBdG, p);
+        // System2D sys(HBdG, p);
 
-        sys._p.mu = meV2au(0.0);
-        sys.setHamiltonian(Hk);
-        auto FS = sys.findFSContours();
-        sys.setHamiltonian(HBdG);
+        // sys._p.mu = meV2au(0.0);
+        // sys.setHamiltonian(Hk);
+        // auto FS = sys.findFSContours();
+        // sys.setHamiltonian(HBdG);
 
-        for (auto ic = 0; ic < FS.size(); ++ic)
-        {
-            sys.printGapAlongContour("data/gap_FS" + std::to_string(ic) + ".dat", FS[ic]);
-        }
+        // for (auto ic = 0; ic < FS.size(); ++ic)
+        // {
+        //     sys.printGapAlongContour("data/gap_FS" + std::to_string(ic) + ".dat", FS[ic]);
+        // }
     }
 
     return 0;
